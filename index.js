9794a1e03b6f1bc541af7dc6c4706bd9df0c8002
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-10-01 18:09:45 +0200

    scraping name and price of a test product

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ const express = require('express');
 
 const app = express();
 
+// url to scrap
+const url = 'https://www.arredamentistramenga.it/cof-cf-s-200';
+
+// scraping the page using promises
+axios(url)
+  .then(response => {
+    // the html of the page requested
+    const HTML = response.data;
+    // passing the html document ot cheerio
+    const $ = cheerio.load(HTML);
+    // looking for a specific element in the page
+    $('.base', HTML).each(function(item) {
+      const title = $(this).text();
+      console.log(title);
+    });
+    // looking for a specific element in the page
+    $('.price', HTML).each(function(item) {
+      const price = $(this).text();
+      console.log(price);
+    });
+  });
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
